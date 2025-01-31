3c44ef81eea97bf5af633cebe3db014ae8d38887
Kshitij <kshitijjha9755@gmail.com>
2025-01-31 19:43:43 +0530

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,71 @@
+
+import { Icon } from "@iconify/react";
+import HeroLight from "./images/hero-light.gif";
+import HeroDark from "./images/hero-dark.gif";
+import Logo from "./images/logo.svg";
+export const githubUsername = "kshitij-kj";
+export const navLogo = null;
+export const Blog = null;
+export { HeroLight as Light };
+export { HeroDark as Dark };
+export const moreInfo =
+  "I am highly proficient in various programming languages, including C++, Python, and SQL, and am constantly striving to improve my skills by tackling new and challenging projects.Currently, I am focused on enhancing my expertise in full-stack development, with a particular interest in building scalable web applications and optimizing user experiences. In addition to technical skills, I value collaboration, and enjoy working in team settings to solve complex problems.As a dedicated learner, I continuously seek opportunities to grow and contribute to the tech community. I am always eager to explore new technologies and methodologies to stay at the forefront of software development trends."
+;
+export const skillData = [
+  {
+    id: 1,
+    skill: <Icon icon="mdi:language-html5" className="display-4" />,
+    name: "HTML5",
+  },
+  {
+    id: 2,
+    skill: <Icon icon="ion:logo-css3" className="display-4" />,
+    name: "CSS3",
+  },
+  {
+    id: 3,
+    skill: <Icon icon="fa6-brands:js" className="display-4" />,
+    name: "JavaScript",
+  },
+  {
+    id: 4,
+    skill: <Icon icon="ri:bootstrap-fill" className="display-4" />,
+    name: "BootStrap",
+  },
+  {
+    id: 5,
+    skill: <Icon icon="mdi:react" className="display-4" />,
+    name: "React",
+  },
+  {
+    id: 6,
+    skill: <Icon icon="file-icons:styledcomponents" className="display-4" />,
+    name: "Styled Components",
+  },
+  {
+    id: 7,
+    skill: <Icon icon="akar-icons:redux-fill" className="display-4" />,
+    name: "Redux",
+  },
+  {
+    id: 8,
+    skill: <Icon icon="bi:git" className="display-4" />,
+    name: "Git",
+  },
+  {
+    id: 9,
+    skill: <Icon icon="fa6-brands:square-github" className="display-4" />,
+    name: "GitHub",
+  },
+];
+
+export const resume = "https://drive.google.com/file/d/1RdmLYZt2_ELxWPQjbR6Y-WpQiHC-om7f/view?usp=drive_link";
+export const filteredProjects = ["example-1", "example-2", "example-3"];
+export const projectCardImages = [
+  {
+    name: "example-1",
+    image: Logo,
+  },
+];
+export const formspreeUrl = "https://formspree.io/f/xeoekazy";
+export const footerTheme = "dark";
